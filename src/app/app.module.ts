6be234cbfcaab1358143b3fe3629c43b09acd575
barbsmfe <barbara.ferreira@acad.pucrs.br>
2018-11-17 18:58:28 -0200

    Footer/Nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavigationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
